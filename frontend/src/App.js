a93212bd73c8f14198477a643ed8e83ac64cd43b
cyro <coolronitchs@gmail.com>
2025-03-22 00:45:25 +0530

    restaurant card and restaurant details page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import './App.css';
-import Navbar from './component/Navbar';
+import Navbar from './component/navbar/Navbar.jsx';
 import { darkTheme } from './Theme/DarkTheme';
 import Home from './component/Home/Home.jsx';
+import RestaurantDetails from './component/restaurant/RestaurantDetails.jsx';
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+      <RestaurantDetails />
+
     </ThemeProvider>
   );
 }
--- frontend/src/component/navbar/Navbar.jsx
+++ frontend/src/component/navbar/Navbar.jsx
@@ -5,16 +5,17 @@ import PersonIcon from '@mui/icons-material/Person';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import IconButton from '@mui/material/IconButton';
 import { Badge } from '@mui/material';
+import './Navbar.css';
 
 
 export default function Navbar() {
     return (
-        <div className='px-5 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between' >
+        <div className='navbar w-full  px-5 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between' >
 
             <div className='lg:mr-10 cursor-pointer flex items-center space-x-4'>
-                <li className='logo font-semibold text-grey-300 text-2xl'>
+                <div className='logo font-semibold text-grey-300 text-2xl'>
                     CraveKart
-                </li>
+                </div>
             </div>
 
             <div className='flex space-x-4'>
