0b93798ddf7bd7ead4c3295c0d258dc61844cfb1
cyro <coolronitchs@gmail.com>
2026-03-14 18:38:57 +0530

    order confirmed pub created

--- backend/core/src/main/java/com/cravekart/core/events/notification/OrderConfirmedEvent.java
+++ backend/core/src/main/java/com/cravekart/core/events/notification/OrderConfirmedEvent.java
@@ -1,4 +1,22 @@
 package com.cravekart.core.events.notification;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderConfirmedEvent {
+
+    private Long orderId;
+    private Long customerId;
+    private String customerName;
+    private String customerEmail;
+    private String restaurantName;
+
+    private String totalPrice;
+    private String estimatedDeliveryTime;
 }
